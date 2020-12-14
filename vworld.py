ebe9f3ea7d0e8c1854826e87299d25ac2ff63d6a
Junhyun Kim <me@junbread.win>
2020-12-14 18:50:18 +0900

    cache 수정

--- vworld.py
+++ vworld.py
@@ -1,5 +1,4 @@
 import requests
-from collections import defaultdict
 import io
 from PIL import Image
 
@@ -7,22 +6,24 @@ class VWorld:
     def __init__(self, apiKey) -> None:
         self.baseURL = 'https://api.vworld.kr/req'
         self.apiKey = apiKey
-        self.cache = defaultdict(str)
+        self.cache = dict()
     
     def get_image(self, x, y) -> Image:
         coords_str = f'{x},{y}'
 
-        response = self.cache[coords_str] \
-            or requests.get(f'{self.baseURL}/image', {
-                'request': 'getmap',
-                'service': 'image',
-                'key': self.apiKey,
-                'zoom': 18,
-                'size': '1024,1024',
-                'crs': 'EPSG:4326',
-                'basemap': 'PHOTO',
-                'center': coords_str,
-            })
+        if coords_str in self.cache:
+            response = self.cache[coords_str]
+        else:
+            response = requests.get(f'{self.baseURL}/image', {
+            'request': 'getmap',
+            'service': 'image',
+            'key': self.apiKey,
+            'zoom': 18,
+            'size': '1024,1024',
+            'crs': 'EPSG:4326',
+            'basemap': 'PHOTO',
+            'center': coords_str,
+        })
 
         image = Image.open(io.BytesIO(response.content))
         return image
