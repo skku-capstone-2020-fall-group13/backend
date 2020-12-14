03468147e0b18d0234f5309ccb5e1658a6230307
Junhyun Kim <me@junbread.win>
2020-12-14 18:49:59 +0900

    import 수정, cache 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 import sys
 import torch
@@ -9,12 +10,13 @@ import PIL.Image as Image
 from unet import UnetPipeline
 from unet.util import cat_to_rgb
 
-from comflogic import logic
+from comflogic import analyze
 
 from vworld import VWorld
 from util import convert_base64
 
 app = Flask(__name__)
+CORS(app)
 
 # load unet model
 unet = UnetPipeline()
@@ -25,31 +27,39 @@ with open('./secrets.json') as secrets_file:
     secrets = json.load(secrets_file)
 vworld = VWorld(secrets['vworldApiKey'])
 
+cache = dict()
+
 @app.route('/analyze', methods = ['GET'])
 def analysis():
     x = request.args.get('x')
     y = request.args.get('y')
-    
-    # get original image from vworld
-    original_img = vworld.get_image(x, y)
-    original_arr = np.array(original_img.convert('RGB'))
-    
-    # get prediction from segmentation model
-    predict_arr = unet.predict(original_arr, batch_size = 8)
-    predict_img = Image.fromarray(cat_to_rgb(predict_arr).astype('uint8'))
+    key_str = f'{x},{y}'
 
-    # get scores from score module
-    scores = logic(predict_arr)
+    if key_str not in cache:
+        # get original image from vworld
+        original_img = vworld.get_image(x, y)
+        original_arr = np.array(original_img.convert('RGB'))
+        
+        # get prediction from segmentation model
+        predict_arr = unet.predict(original_arr, batch_size = 8)
+        predict_img = Image.fromarray(cat_to_rgb(predict_arr).astype('uint8'))
 
-    # convert images to base64
-    original_img_base64 = convert_base64(original_img)
-    predict_img_base64 = convert_base64(predict_img)
+        # get scores from score module
+        scores = analyze(predict_arr)
 
-    return jsonify({
-        'original_image': original_img_base64,
-        'predict_image': predict_img_base64,
-        'scores': scores,
-    })
+        # convert images to base64
+        original_img_base64 = convert_base64(original_img)
+        predict_img_base64 = convert_base64(predict_img)
+
+        response = {
+            'original_image': original_img_base64,
+            'predict_image': predict_img_base64,
+            'scores': scores,
+        }
+
+        cache[key_str] = response
+
+    return cache[key_str]
 
 if __name__ == "__main__" :
     
